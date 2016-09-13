4444cc1d41831b9479f93c4f7a8c63a92bd85174
Rui Dong <rui.dong.jh@gmail.com>
2016-09-13 15:08:41 -0400

    tried toggle and if, light turns on and off, text changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+$(function(){
+	// setting a variable to store if the lights are on or off
+	var lights = 'on';
 
+	// event listener (click) for the light switch (#switch) button
+	$('#switch').click( function(){ switchLights() });
 
+	function switchLights(){
+    $('body').toggleClass('dark')
+		if( lights === 'on' ){
+			// $('body').toggleClass('dark');
+      $('.status').text("It's so dark in here!")
+			lights = 'off';
+		}else{
+      // $('body').toggleClass('light');
+      $('.status').text("It's so bright in here!")
+			lights = 'on';
+		}// END if( lights === 'on' )
+	}
+});
